826b74321b76bb533bf0bf213b8dea53c5b6b813
gitpeut <peut@peut.org>
2021-06-19 23:32:52 +0200

    math ready

--- math-methods.js
+++ math-methods.js
@@ -9,18 +9,20 @@ console.log(Math.PI);
 // Tip: de formule is omtrek = diameter * pi
 // ---- Verwachte uitkomst: 12.566370614359172
 
+console.log( Math.PI * 4 );
 
 /* Opdracht 2: */
 // Bereken de omtrek van een cirkel met een diameter van 12 en rond dit af naar een heel getal. Log dit in de terminal.
 // ---- Verwachte uitkomst: 38
 
+console.log ( Math.round(Math.PI * 12)  );
 
 
 /* Opdracht 3: */
 // Bereken de wortel van 9 en log dit in de terminal
 // ---- Verwachte uitkomst: 3
 
-
+console.log ( Math.sqrt(9));
 
 /* Opdracht 4: */
 // Genereer een willekeurig getal tussen de 0 en 1 en doe dan een check:
@@ -28,3 +30,10 @@ console.log(Math.PI);
 // * is het willekeurige getal kleiner dan 0.5 ? Log dan het getal met "Het glas is half leeg" in de terminal
 // Tip: iedere keer als je het script opnieuw draait (wel even een extra spatie toevoegen o.i.d.) is dit getal anders ;)
 // ---- Verwachte uitkomst: Bijbehorende zin afhankelijk van de uitkomst
+
+let randomNUmber = Math.random();
+if ( randomNUmber > 0.5 ){
+    console.log( 'Het glas is half vol.');
+}else{
+    console.log( 'Het glas is half leeg.');
+}
