884fedf7e46286daf2cef6a717401518d06fc8fa
gitpeut <peut@peut.org>
2021-06-20 00:35:38 +0200

    date ready

--- date-methods.js
+++ date-methods.js
@@ -3,7 +3,7 @@
 // ---- Verwachte uitkomst: 2021-04-06T14:54:34.062Z (afhankelijk van het moment)
 
 const currentDay = new Date();
-console.log(currentDay);
+console.log(currentDay.toLocaleString() );
 
 
 /* Opdracht 1 */
@@ -13,8 +13,38 @@ console.log(currentDay);
 // Uren: 16 (als er een vier in de klok zit op het moment van aanroepen)
 // Minuten: 36 (als het 16:36 is op het moment van aanroepen)
 
+const nowDate = new Date();
+
+console.log( nowDate.toTimeString() );
+console.log( nowDate.getHours() );
+console.log( nowDate.getMinutes() );
+
 
 
 /* Opdracht 2 */
 // Format het date-object dat je bij de vorige vraag hebt gemaakt naar een leesbare datum
 // ---- Verwachte uitkomst: Tue Apr 06 2021 (afhankelijk van het moment)
+
+const longOptions = {
+    weekday: 'short',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+};
+
+// Using the english locale produces commas after the weekday and the date
+let enDateWithCommas = currentDay.toLocaleString( 'en-EN', longOptions );
+
+//These commas are unwanted, so must be filtered out
+console.log( 'Date with commas ' + enDateWithCommas);
+enCommalessArray = enDateWithCommas.split(',');
+console.log( enCommalessArray);
+enDateWithoutCommas = enCommalessArray.join('');
+
+console.log( enDateWithoutCommas);
+
+// But , much easier, use the toDateString() method, that does not have commas,
+
+console.log ( currentDay.toDateString());
+
+
